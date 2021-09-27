632870d0e11ee05f9d4f30e4bc1d6b60ea7ead7d
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2021-09-27 21:30:33 +0200

    2021_07_10-Praha

---
+++ 2021_07_10-Praha/render.bat
@@ -0,0 +1,4 @@
+"D:\Program Files\VirtualDub\vdub64.exe" /s d:\Users\ilx\wrk\video\2021_07_10-Praha\20210710_170831.mp4.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s d:\Users\ilx\wrk\video\2021_07_10-Praha\20210710_170910.mp4.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s d:\Users\ilx\wrk\video\2021_07_10-Praha\20210710_170924.mp4.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s d:\Users\ilx\wrk\video\2021_07_10-Praha\20210710_171002.mp4.vdscript
