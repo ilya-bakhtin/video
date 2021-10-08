c13c25b5a80a267a44637cfb98eb093a0e03fdb4
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2021-10-08 18:56:54 +0200

    2021_09_28-Hallstatt

---
+++ 2021_09_28-Hallstatt/render.bat
@@ -0,0 +1,6 @@
+"D:\Program Files\VirtualDub\vdub64.exe" /s d:\Users\ilx\wrk\video\2021_09_28-Hallstatt\20210928_133550.mp4.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s d:\Users\ilx\wrk\video\2021_09_28-Hallstatt\20210928_133618.mp4.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s d:\Users\ilx\wrk\video\2021_09_28-Hallstatt\20210928_134532.mp4.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s d:\Users\ilx\wrk\video\2021_09_28-Hallstatt\20210928_134937.mp4.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s d:\Users\ilx\wrk\video\2021_09_28-Hallstatt\20210928_151758.mp4.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s d:\Users\ilx\wrk\video\2021_09_28-Hallstatt\20210928_153241.mp4.vdscript
