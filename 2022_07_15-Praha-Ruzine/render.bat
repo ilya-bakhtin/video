8178f799bff4661c5ccfb65a9cfc4524dc0b50f0
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2022-07-23 19:33:00 +0200

    2022_07_15-Praha-Ruzine

---
+++ 2022_07_15-Praha-Ruzine/render.bat
@@ -0,0 +1,7 @@
+"D:\Program Files\VirtualDub\vdub64.exe" /s d:\Users\ilx\wrk\video\2022_07_15-Praha-Ruzine\VID_20220715_172730.mp4.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s d:\Users\ilx\wrk\video\2022_07_15-Praha-Ruzine\VID_20220715_172816.mp4.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s d:\Users\ilx\wrk\video\2022_07_15-Praha-Ruzine\VID_20220715_172907.mp4.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s d:\Users\ilx\wrk\video\2022_07_15-Praha-Ruzine\VID_20220715_172918.mp4.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s d:\Users\ilx\wrk\video\2022_07_15-Praha-Ruzine\VID_20220715_173352.mp4.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s d:\Users\ilx\wrk\video\2022_07_15-Praha-Ruzine\VID_20220715_173445.mp4.vdscript
+"D:\Program Files\VirtualDub\vdub64.exe" /s d:\Users\ilx\wrk\video\2022_07_15-Praha-Ruzine\VID_20220715_173542.mp4.vdscript
