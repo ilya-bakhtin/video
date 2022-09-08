b1569a3f3ba87528a4f30fb3dc6052bb99b82922
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2022-09-08 15:12:29 +0200

    2022_09_04-Praha

---
+++ 2022_09_04-Praha/render-1-upd.bat
@@ -0,0 +1,5 @@
+call render-step.bat M:\Media\video\2022_09_04-Praha\result 20220904_162357.mp4
+call render-step.bat M:\Media\video\2022_09_04-Praha\result 20220904_162923.mp4
+call render-step.bat M:\Media\video\2022_09_04-Praha\result 20220904_164737.mp4
+call render-step.bat M:\Media\video\2022_09_04-Praha\result 20220904_164915.mp4
+call render-step.bat M:\Media\video\2022_09_04-Praha\result 20220904_164948.mp4
---
+++ 2022_09_04-Praha/render-1.bat
@@ -0,0 +1,26 @@
+call render-step.bat M:\Media\video\2022_09_04-Praha\result 20220904_162357.mp4
+
+call render-step.bat M:\Media\video\2022_09_04-Praha\result 20220904_162923.mp4
+
+call render-step.bat M:\Media\video\2022_09_04-Praha\result 20220904_162947.mp4
+
+call render-step.bat M:\Media\video\2022_09_04-Praha\result 20220904_163608.mp4
+
+call render-step.bat M:\Media\video\2022_09_04-Praha\result 20220904_164053.mp4
+
+call render-step.bat M:\Media\video\2022_09_04-Praha\result 20220904_164737.mp4
+
+call render-step.bat M:\Media\video\2022_09_04-Praha\result 20220904_164915.mp4
+
+call render-step.bat M:\Media\video\2022_09_04-Praha\result 20220904_164948.mp4
+
+call render-step.bat M:\Media\video\2022_09_04-Praha\result 20220904_165033.mp4
+
+call render-step.bat M:\Media\video\2022_09_04-Praha\result 20220904_165218.mp4
+
+call render-step.bat M:\Media\video\2022_09_04-Praha\result 20220904_165839.mp4
+
+call render-step.bat M:\Media\video\2022_09_04-Praha\result 20220904_165914.mp4
+
+call render-step.bat M:\Media\video\2022_09_04-Praha\result 20220904_170042.mp4
+
