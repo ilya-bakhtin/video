d58d67fe863c0144ba96b16c1c89efecfa5df6f5
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2022-12-25 09:16:46 +0100

    2022_10_30-Vranov-nad-Dyjí

---
+++ 2022_10_30-Vranov-nad-Dyjí/render-1.bat
@@ -0,0 +1,100 @@
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221028_133828.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221028_133858.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221028_134557.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221028_155602.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221028_155657.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221028_155731.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221028_160617.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_103207.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_103244.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_103347.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_103407.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_104958.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_105020.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_110518.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_110849.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_114632.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_114931.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_115247.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_115631.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_115658.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_115846.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_135516.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_135712.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_135730.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_140508.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_141211.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_141453.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_141523.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_141544.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_141754.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_144224.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_144248.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_145050.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_145234.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_145418.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_145703.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221029_150304.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221030_110508.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221030_110733.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221030_162029.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221030_162107.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221030_182046.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221030_182113.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221030_185558.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221030_195533.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221030_195544.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221030_195707.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221030_195850.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221030_195924.mp4
+
+call render-step.bat M:\Media\video\2022_10_30-Vranov-nad-Dyjí\ 20221030_195954.mp4
+
