f526fd09985e7678cc726ab8a9fd51fd0ca28fa9
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2023-02-14 18:21:16 +0100

    2023_01_20-Pariz

---
+++ 2023_01_20-Pariz/render-1.bat
@@ -0,0 +1,204 @@
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230117_090854.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230117_090928.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230118_111339.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230118_114029.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230118_114819.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230118_115114.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230118_121228.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230118_121320.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230118_122021.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230118_122309.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230118_122713.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230118_124704.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230118_140035.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230118_140200.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230118_140224.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230118_140342.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230118_140419.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230118_140812.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230118_140827.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230118_150049.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230118_150345.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230118_151012.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230118_151350.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230118_152300.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230119_125651.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230119_130300.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230119_132200.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230119_154318.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230119_154339.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230119_154415.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230119_155817.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230119_160814.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230119_162427.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_121136.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_122006.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_122231.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_122739.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_124227.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_124744.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_124800.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_124925.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_125050.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_125202.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_125455.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_125527.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_125604.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_125844.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_130242.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_135258.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_135502.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_140716.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_140822.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_140949.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_141025.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_141122.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_141141.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_141340.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_141802.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_141834.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_141904.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_142008.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_142933.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_151448.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_152405.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_152453.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_152512.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_152843.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_152928.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_153107.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_153209.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_153242.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_153328.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_153645.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_153713.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_153741.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_153801.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_154133.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_154151.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_154339.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_154448.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_154616.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_155104.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_155136.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_155315.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_155411.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_155440.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_155533.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_155619.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_155742.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_160656.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_160724.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_160748.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_160810.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_161245.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_161525.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_165620.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_165923.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_170141.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_170202.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_170345.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_170420.mp4
+
+call render-step.bat M:\Media\video\2023_01_20-Pariz\ 20230120_170927.mp4
+
