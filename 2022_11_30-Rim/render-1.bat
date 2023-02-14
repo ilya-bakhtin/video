dd8c56e6f97902321c5158eba1761c7704460c80
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2023-02-14 18:17:03 +0100

    2022_11_30-Rim

---
+++ 2022_11_30-Rim/render-1.bat
@@ -0,0 +1,304 @@
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221126_094204.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221126_094423.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221126_094454.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221126_094510.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221126_122848.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221126_122947.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221126_123411.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221126_123451.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221126_123540.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221126_151414.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221126_151458.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221126_151512.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_090941.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_102038.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_102116.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_102147.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_102219.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_102253.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_102321.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_102817.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_104741.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_104806.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_105308.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_105443.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_105722.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_105849.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_110027.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_110043.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_110149.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_110645.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_110835.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_110902.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_110921.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_111935.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_112147.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_112219.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_112237.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_114040.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_114058.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_114230.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_114331.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_114939.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_115735.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_115900.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_120621.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_120645.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_121634.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_121649.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_123531.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_125646.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_130753.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_131026.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_131057.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_131440.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_131501.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_131535.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_131557.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_131656.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_131815.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_131948.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_132256.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_132502.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_132541.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_133753.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_135637.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_135709.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_135823.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_140146.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_141247.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_141308.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_141448.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_141504.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_160904.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_161046.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221127_161816.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_110950.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_113138.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_113940.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_114001.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_114027.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_114052.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_114303.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_114347.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_120819.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_121051.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_121932.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_122253.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_122821.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_122914.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_130829.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_130906.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_131511.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_160058.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_160149.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_171818.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_172158.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_172313.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_172519.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_172735.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221128_173758.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_115551.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_121234.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_121312.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_121338.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_121422.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_121545.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_121912.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_121929.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_122141.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_122158.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_122222.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_122303.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_122343.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_123109.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_123351.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_123518.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_123536.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_123558.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_123632.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_124148.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_124219.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_124237.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_124337.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_124359.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_124744.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_124822.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_124854.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_125256.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_125411.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_125432.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_135843.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_135911.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_135942.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_140034.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_140145.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_152524.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_152648.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_152713.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_152807.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_162151.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_163457.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_164429.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_170842.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_171734.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_171922.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_172519.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_172753.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_173158.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_173336.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_173357.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_173942.mp4
+
+call render-step.bat M:\Media\video\2022_11_30-Rim\ 20221129_174014.mp4
+
