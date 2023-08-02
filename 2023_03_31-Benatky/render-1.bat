b8833a6b8a904a6515285e39750908b822641e34
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2023-08-02 18:58:40 +0200

    2023_03_31-Benatky

---
+++ 2023_03_31-Benatky/render-1.bat
@@ -0,0 +1,190 @@
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230327_095311.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230327_115347.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230327_131700.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230327_131726.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230327_131748.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230327_132032.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230327_132200.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230327_134244.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230327_142350.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230327_171743.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_103608.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_103640.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_103719.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_104042.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_104233.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_104303.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_104340.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_110724.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_110909.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_111226.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_112401.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_112513.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_112554.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_114327.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_121943.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_122011.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_122035.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_123556.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_131613.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_131651.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_131707.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_132033.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_132211.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_132735.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_132756.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_133004.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_133303.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_141817.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_142342.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_142612.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_142725.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_142932.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_142950.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_143252.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_151909.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_152201.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_152827.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_152848.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_153441.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_153818.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_153950.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_155852.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230328_161609.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230329_115013.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230329_115038.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230329_120134.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230329_122301.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230329_122354.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230329_122559.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230329_124352.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230329_124417.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230329_124438.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230329_124510.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230329_124643.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230329_172530.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_101951.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_110734.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_113840.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_113944.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_114041.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_114158.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_123156.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_123233.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_131432.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_131531.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_131551.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_131640.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_132426.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_133037.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_133119.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_133240.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_143045.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_143246.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_161006.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_161152.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_161229.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_164946.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_165249.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_165326.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_165346.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_171413.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_171454.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_171618.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230330_174633.mp4
+
+call render-step.bat M:\Media\video\2023_03_31-Benatky\ 20230331_074028.mp4
+
