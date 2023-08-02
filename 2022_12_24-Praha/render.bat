525256d3efa922d03e2f7d06290ceba08cedfdda
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2023-08-02 18:53:18 +0200

    2022_12_24-Praha

---
+++ 2022_12_24-Praha/render.bat
@@ -0,0 +1,72 @@
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221223_153323.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\PXL_20221224_145719922.TS.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_153351.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_153412.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_153744.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_153858.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_153946.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_154006.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_154039.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_154129.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_154142.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_154204.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_154225.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_154540.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_154928.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_155450.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_155558.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_160240.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_160303.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_160325.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_160353.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_160617.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_160656.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_161031.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_161306.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_161357.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_161429.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_161443.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_162235.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_162346.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_162638.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_162738.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_163001.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_163625.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_164545.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2022_12_24-Praha\20221224_165943.mp4.vdscript
+
