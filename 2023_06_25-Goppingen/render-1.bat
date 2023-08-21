211182b0acffb2e37544bff55ecda3fcac5dd05f
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2023-08-21 08:34:54 +0200

    2023_06_25-Goppingen

---
+++ 2023_06_25-Goppingen/render-1.bat
@@ -0,0 +1,30 @@
+call render-step.bat M:\Media\video\2023_06_25-Goppingen\ 20230625_132922.mp4
+
+call render-step.bat M:\Media\video\2023_06_25-Goppingen\ 20230625_134422.mp4
+
+call render-step.bat M:\Media\video\2023_06_25-Goppingen\ 20230625_134623.mp4
+
+call render-step.bat M:\Media\video\2023_06_25-Goppingen\ 20230625_135545.mp4
+
+call render-step.bat M:\Media\video\2023_06_25-Goppingen\ 20230625_135640.mp4
+
+call render-step.bat M:\Media\video\2023_06_25-Goppingen\ 20230625_135751.mp4
+
+call render-step.bat M:\Media\video\2023_06_25-Goppingen\ 20230625_141743.mp4
+
+call render-step.bat M:\Media\video\2023_06_25-Goppingen\ 20230625_141847.mp4
+
+call render-step.bat M:\Media\video\2023_06_25-Goppingen\ 20230625_141909.mp4
+
+call render-step.bat M:\Media\video\2023_06_25-Goppingen\ 20230625_141942.mp4
+
+call render-step.bat M:\Media\video\2023_06_25-Goppingen\ 20230625_142008.mp4
+
+call render-step.bat M:\Media\video\2023_06_25-Goppingen\ 20230625_142030.mp4
+
+call render-step.bat M:\Media\video\2023_06_25-Goppingen\ 20230625_142053.mp4
+
+call render-step.bat M:\Media\video\2023_06_25-Goppingen\ 20230625_142115.mp4
+
+call render-step.bat M:\Media\video\2023_06_25-Goppingen\ 20230625_142912.mp4
+
---
+++ 2023_06_25-Goppingen/render.bat
@@ -0,0 +1,30 @@
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_25-Goppingen\20230625_132922.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_25-Goppingen\20230625_134422.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_25-Goppingen\20230625_134623.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_25-Goppingen\20230625_135545.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_25-Goppingen\20230625_135640.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_25-Goppingen\20230625_135751.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_25-Goppingen\20230625_141743.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_25-Goppingen\20230625_141847.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_25-Goppingen\20230625_141909.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_25-Goppingen\20230625_141942.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_25-Goppingen\20230625_142008.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_25-Goppingen\20230625_142030.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_25-Goppingen\20230625_142053.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_25-Goppingen\20230625_142115.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_25-Goppingen\20230625_142912.mp4.vdscript
+
