e7cde1dfefa98468c84fa14b938c716d97264ee9
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2023-08-21 08:32:38 +0200

    2023_06_23-Praha-Goppingen-ruze

---
+++ 2023_06_23-Praha-Goppingen-ruze/render-1.bat
@@ -0,0 +1,18 @@
+call render-step.bat M:\Media\video\2023_06_23-Praha-Goppingen-ruze\ 20230617_133506.mp4
+
+call render-step.bat M:\Media\video\2023_06_23-Praha-Goppingen-ruze\ 20230617_140004.mp4
+
+call render-step.bat M:\Media\video\2023_06_23-Praha-Goppingen-ruze\ 20230617_144746.mp4
+
+call render-step.bat M:\Media\video\2023_06_23-Praha-Goppingen-ruze\ 20230623_124807.mp4
+
+call render-step.bat M:\Media\video\2023_06_23-Praha-Goppingen-ruze\ 20230623_124824.mp4
+
+call render-step.bat M:\Media\video\2023_06_23-Praha-Goppingen-ruze\ 20230623_130138.mp4
+
+call render-step.bat M:\Media\video\2023_06_23-Praha-Goppingen-ruze\ 20230623_130242.mp4
+
+call render-step.bat M:\Media\video\2023_06_23-Praha-Goppingen-ruze\ 20230623_131708.mp4
+
+call render-step.bat M:\Media\video\2023_06_23-Praha-Goppingen-ruze\ 20230623_132319.mp4
+
---
+++ 2023_06_23-Praha-Goppingen-ruze/render.bat
@@ -0,0 +1,18 @@
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_23-Praha-Goppingen-ruze\20230617_133506.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_23-Praha-Goppingen-ruze\20230617_140004.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_23-Praha-Goppingen-ruze\20230617_144746.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_23-Praha-Goppingen-ruze\20230623_124807.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_23-Praha-Goppingen-ruze\20230623_124824.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_23-Praha-Goppingen-ruze\20230623_130138.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_23-Praha-Goppingen-ruze\20230623_130242.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_23-Praha-Goppingen-ruze\20230623_131708.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_06_23-Praha-Goppingen-ruze\20230623_132319.mp4.vdscript
+
