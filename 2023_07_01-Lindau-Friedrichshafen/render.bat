5611635940cf4838a0bcfd182854bbf57e628f5f
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2023-08-22 13:05:05 +0200

    2023_07_01-Lindau-Friedrichshafen

---
+++ 2023_07_01-Lindau-Friedrichshafen/render.bat
@@ -0,0 +1,114 @@
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_125159.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_125354.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_125509.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_125838.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_132026.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_142104.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_142144.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_142401.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_143821.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_143913.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_143949.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_144153.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_144225.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_144516.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_144618.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_144644.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_145226.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_151432.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_151529.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_151553.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_152158.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_152405.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_163509.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_163555.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_163813.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_164029.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_164047.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_164446.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_165820.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_165857.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_170725.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_170958.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_171908.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_173122.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_173158.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_173300.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_173314.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_180317.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230701_180350.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230702_132143.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230702_140844.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230702_141133.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230702_141235.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230702_141309.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230702_161917.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230702_161933.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230702_162017.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230702_162242.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230702_162316.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230702_162340.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230702_162549.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230702_162622.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230702_162646.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230702_162908.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230702_163439.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230702_163603.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_01-Lindau-Friedrichshafen\20230702_163629.mp4.vdscript
+
