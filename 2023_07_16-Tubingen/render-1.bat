30639af1ea3e52274380a57fd35fbe56df2fabd8
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2023-08-30 14:33:30 +0200

    2023_07_16-Tubingen

---
+++ 2023_07_16-Tubingen/render-1.bat
@@ -0,0 +1,76 @@
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_143429.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_143509.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_143916.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_144015.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_144045.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_144156.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_144308.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_144330.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_144358.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_144423.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_144448.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_144508.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_144851.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_144935.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_145018.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_145103.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_145159.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_145516.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_145619.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_145810.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_154330.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_155132.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_155529.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_155602.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_155621.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_155945.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_160034.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_160244.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_160310.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_160329.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_160356.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_160452.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_160512.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_160644.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_165433.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_165502.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_165851.mp4
+
+call render-step.bat M:\Media\video\2023_07_16-Tubingen\ 20230716_165928.mp4
+
