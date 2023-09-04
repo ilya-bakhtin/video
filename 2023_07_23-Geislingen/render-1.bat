b99fd1d7b5dd9285c72e2cd2ffeb2cca9a9d4b91
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2023-09-04 13:02:31 +0200

    2023_07_23-Geislingen

---
+++ 2023_07_23-Geislingen/render-1.bat
@@ -0,0 +1,56 @@
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_133936.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_134011.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_134028.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_134333.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_134432.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_134726.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_134813.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_135026.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_135152.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_135229.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_135338.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_135442.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_135850.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_140057.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_140306.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_140520.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_140659.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_140748.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_141004.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_141043.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_141157.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_143949.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_144327.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_144413.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_145015.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_145111.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_150949.mp4
+
+call render-step.bat M:\Media\video\2023_07_23-Geislingen\ 20230723_152132.mp4
+
---
+++ 2023_07_23-Geislingen/render.bat
@@ -0,0 +1,56 @@
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_133936.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_134011.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_134028.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_134333.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_134432.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_134726.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_134813.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_135026.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_135152.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_135229.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_135338.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_135442.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_135850.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_140057.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_140306.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_140520.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_140659.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_140748.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_141004.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_141043.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_141157.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_143949.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_144327.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_144413.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_145015.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_145111.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_150949.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_07_23-Geislingen\20230723_152132.mp4.vdscript
+
