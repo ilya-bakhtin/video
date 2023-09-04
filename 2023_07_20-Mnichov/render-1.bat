57ee1b20275c48a42204fd95e9439abd5d2db062
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2023-09-04 13:00:44 +0200

    2023_07_20-Mnichov

---
+++ 2023_07_20-Mnichov/render-1.bat
@@ -0,0 +1,84 @@
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230720_160854.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230720_160916.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230720_160951.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230720_161020.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230720_161652.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230720_174056.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230720_174318.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230720_174508.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230720_174815.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230720_174845.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_122444.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_122526.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_122724.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_122801.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_122828.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_122850.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_123000.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_123025.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_123114.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_123253.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_123314.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_134945.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_140439.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_140849.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_142925.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_142956.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_143022.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_143459.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_143614.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_143645.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_143712.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_145308.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_150507.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_152752.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_152822.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_152850.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230721_153445.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230722_115145.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230722_115222.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230722_115244.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230722_120458.mp4
+
+call render-step.bat M:\Media\video\2023_07_20-Mnichov\ 20230722_120514.mp4
+
