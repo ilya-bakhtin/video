95409fb62f9992f4068282bbe6dee63d043e19af
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2023-09-04 13:04:01 +0200

    2023_09_03-Praha

---
+++ 2023_09_03-Praha/render-1.bat
@@ -0,0 +1,20 @@
+call render-step.bat M:\Media\video\2023_09_03-Praha\ 20230903_135940.mp4
+
+call render-step.bat M:\Media\video\2023_09_03-Praha\ 20230903_135956.mp4
+
+call render-step.bat M:\Media\video\2023_09_03-Praha\ 20230903_140037.mp4
+
+call render-step.bat M:\Media\video\2023_09_03-Praha\ 20230903_140123.mp4
+
+call render-step.bat M:\Media\video\2023_09_03-Praha\ 20230903_140203.mp4
+
+call render-step.bat M:\Media\video\2023_09_03-Praha\ 20230903_140420.mp4
+
+call render-step.bat M:\Media\video\2023_09_03-Praha\ 20230903_140436.mp4
+
+call render-step.bat M:\Media\video\2023_09_03-Praha\ 20230903_142659.mp4
+
+call render-step.bat M:\Media\video\2023_09_03-Praha\ 20230903_142945.mp4
+
+call render-step.bat M:\Media\video\2023_09_03-Praha\ 20230903_143052.mp4
+
