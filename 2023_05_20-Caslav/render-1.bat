61369c96a1b7a8509793ff80478709ffbc804623
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2024-09-30 18:15:04 +0200

    2023_05_20-Caslav

---
+++ 2023_05_20-Caslav/render-1.bat
@@ -0,0 +1,56 @@
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_114552.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_115309.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_115325.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_115336.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_120201.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_121538.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_121621.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_121730.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_121807.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_122431.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_122853.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_134254.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_134311.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_134343.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_134416.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_134534.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_135738.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_140006.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_140028.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_140054.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_140105.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_140218.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_140243.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_140348.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_140640.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_140705.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_140730.mp4
+
+call render-step.bat M:\Media\video\2023_05_20-Caslav\ 20230520_141346.mp4
+
