5003e90f827f7a43ef843cfb0a17d21d41b566f1
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2024-09-30 18:08:10 +0200

    2023-Praha-zima-jaro

---
+++ 2023-Praha-zima-jaro/render-1.bat
@@ -0,0 +1,112 @@
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230103_153823.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230122_153621.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230122_153743.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230122_153757.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230325_121504.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230326_131501.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230326_131607.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230326_131629.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230326_131646.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230326_133237.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230326_133405.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230326_133438.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230415_144350.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230415_144406.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230416_140629.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230416_142513.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230416_142842.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230416_142914.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230422_132647.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230422_155006.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230422_155030.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230422_155051.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230422_155253.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230422_155313.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230422_155332.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230422_160705.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230422_161254.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230422_162605.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230422_163333.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230423_151618.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230423_152906.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230423_154706.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230423_154738.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230430_132356.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230430_132449.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230430_132530.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230430_132641.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230430_132714.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230430_143229.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230430_143749.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230430_150801.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230430_151449.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230430_151510.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230430_152326.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230430_152430.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230430_153120.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230430_153140.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230501_151330.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230501_151407.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230501_151434.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230501_153123.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230506_140857.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230508_113656.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230521_143248.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230521_143603.mp4
+
+call render-step.bat M:\Media\video\2023-Praha-zima-jaro\ 20230521_144438.mp4
+
