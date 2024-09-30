f7743a359a36e7648f8a0dcca618a41a30b84b65
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2024-09-30 18:16:28 +0200

    2023_10_16-Goppingen

---
+++ 2023_10_16-Goppingen/render-1.bat
@@ -0,0 +1,20 @@
+call render-step.bat m:\media\video\2023_10_16-Goppingen\ 20231014_201503.mp4
+
+call render-step.bat m:\media\video\2023_10_16-Goppingen\ 20231014_201545.mp4
+
+call render-step.bat m:\media\video\2023_10_16-Goppingen\ 20231015_175759.mp4
+
+call render-step.bat m:\media\video\2023_10_16-Goppingen\ 20231015_180041.mp4
+
+call render-step.bat m:\media\video\2023_10_16-Goppingen\ 20231017_162107.mp4
+
+call render-step.bat m:\media\video\2023_10_16-Goppingen\ 20231017_162159.mp4
+
+call render-step.bat m:\media\video\2023_10_16-Goppingen\ 20231017_171433.mp4
+
+call render-step.bat m:\media\video\2023_10_16-Goppingen\ 20231017_171556.mp4
+
+call render-step.bat m:\media\video\2023_10_16-Goppingen\ 20231017_171654.mp4
+
+call render-step.bat m:\media\video\2023_10_16-Goppingen\ 20231017_173019.mp4
+
---
+++ 2023_10_16-Goppingen/render.bat
@@ -0,0 +1,20 @@
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_10_16-Goppingen\20231014_201503.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_10_16-Goppingen\20231014_201545.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_10_16-Goppingen\20231015_175759.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_10_16-Goppingen\20231015_180041.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_10_16-Goppingen\20231017_162107.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_10_16-Goppingen\20231017_162159.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_10_16-Goppingen\20231017_171433.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_10_16-Goppingen\20231017_171556.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_10_16-Goppingen\20231017_171654.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2023_10_16-Goppingen\20231017_173019.mp4.vdscript
+
