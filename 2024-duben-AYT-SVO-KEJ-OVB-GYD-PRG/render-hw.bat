47322b84e6045f21d976da54047cbb8bfba5a52d
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2024-11-03 09:19:14 +0100

    2024-duben

---
+++ 2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG/render-hw.bat
@@ -0,0 +1,21 @@
+SET dir=%1
+SET dir_ds=%dir:\=\\%
+SET dir_f=%dir:m:\=e:\%
+SET dir_f=%dir_f:video\=video\tmp\%
+SET dir_f_ds=%dir_f:\=\\%
+
+rem "cmd.exe" /c ""D:\Program Files\MeGUI\tools\ffmpeg\ffmpeg.exe" -loglevel level+error -i "%2.avs" -strict -1 -f yuv4mpegpipe - | "D:\Program Files\MeGUI\tools\x265\vs2019\x265.exe" --crf 12 --preset=slow --sar 1:1 -o "%1\%2.h265" --y4m -"
+rem "cmd.exe" /c ""C:\Users\ilx\wrk\ffmpeg\ffmpeg.exe"             -loglevel level+error -i "%2.avs" -strict -1 -f yuv4mpegpipe - | "C:\users\ilx\wrk\x265\x265.exe"                    --crf 12 --preset=slow --sar 1:1 -o "%1\result\%2.h265" --y4m -"
+rem "D:\Program Files\MeGUI\tools\ffmpeg\ffmpeg.exe" -hwaccel qsv   -i "%2.avs" -c:v hevc_qsv -preset veryslow -global_quality 23           -y "%1\result\%2.h265"
+    "C:\Users\ilx\wrk\ffmpeg\ffmpeg.exe"             -hwaccel dxva2 -i "%2.avs" -c:v hevc_amf -quality 0 -level 6 -rc cqp -qp_i 36 -qp_p 36 -y "%1\result\%2.h265"
+
+rem "D:\Program Files\MeGUI\tools\ffmpeg\ffmpeg.exe" -i "%2.avs" -vn -c:a aac -b:a 384k "%1\result\%2.m4a"
+    "C:\Users\ilx\wrk\ffmpeg\ffmpeg.exe"             -i "%2.avs" -vn -c:a aac -b:a 384k "%1\result\%2.m4a"
+
+mkdir "%dir_f%"
+rem "D:\Program Files\MeGUI\tools\mp4box\mp4box.exe" -add "%1\result\%2.h265#trackID=1:fps=60.0:par=1:1:name=" -add "%1\result\%2.m4a#trackID=1:name=" -tmp "%dir_f_ds%" -new "%1\result\%2.mp4"
+    "C:\Program Files\GPAC\mp4box.exe"               -add "%1\result\%2.h265#trackID=1:fps=60.0:par=1:1:name=" -add "%1\result\%2.m4a#trackID=1:name=" -tmp "%dir_f_ds%" -new "%1\result\%2.mp4"
+rmdir /S /Q "%dir_f%"
+
+del "%1\result\%2.h265"
+del "%1\result\%2.m4a"
---
+++ 2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG/render.bat
@@ -0,0 +1,134 @@
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240403_132043.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240403_143040.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240403_143315.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240403_143901.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240403_144332.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240403_153117.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240403_161344.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240403_161403.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240403_161507.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240403_201025.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240404_050626.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_114607.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_114649.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_114751.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_114938.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_115242.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_124757.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_133711.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_145352.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_145508.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_145534.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_145643.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_145710.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_145846.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_145949.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_160418.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_160739.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_161129.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_162939.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_163926.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_164417.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_164429.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_164443.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_164456.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_164547.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240411_202752.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240412_113908.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240412_124819.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240412_124845.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240412_124918.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240412_135829.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240412_135853.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240412_175610.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_002838.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_053825.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_060538.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_060649.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_060719.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_061312.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_063257.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_064733.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_064821.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_064853.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_064924.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_072310.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_072341.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_072735.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_073034.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_073309.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_073408.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_074316.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_081615.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_100555.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_101714.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_101752.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_113113.mp4.vdscript
+
+"C:\Program Files\VirtualDub\vdub64.exe" /s c:\Users\ilx\wrk\video\2024-duben-AYT-SVO-KEJ-OVB-GYD-PRG\20240413_113726.mp4.vdscript
+
